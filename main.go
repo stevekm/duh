aba90a84a84ad5b716975aec5c6f0b1d1ce78844
Stephen Kelly <stevekm@users.noreply.github.com>
2022-08-12 09:27:13 -0400

    add benchmark dirs makefile and update main methods

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
-	// "log"
+	"log"
 	"path/filepath"
 	"strings"
 )
@@ -24,15 +24,16 @@ func DirSize(dirPath string) (int64, error) {
 	})
 	return size, err
 }
-
 // size, err := DirSize(startDir)
 // if err != nil {
 // 		log.Fatal(err)
 // }
 
+
+
 // get the size of all subdirs
 // https://stackoverflow.com/questions/71153302/how-to-set-depth-for-recursive-iteration-of-directories-in-filepath-walk-func
-func SubDirSizes(subDirPath string) error {
+func SubDirSizes(subDirPath string) (map[string]int64, error) {
 	dirSizes := map[string]int64{}
 	maxDepth := 0
 	err := filepath.Walk(subDirPath, func(path string, info fs.FileInfo, err error) error {
@@ -54,10 +55,32 @@ func SubDirSizes(subDirPath string) error {
 
 	fmt.Printf("dirSizes: %v\n", dirSizes)
 
-	return err
+	return dirSizes, err
 }
 
+// func FormatSubdirStr(dirMap map[string]int64, curDir string) string {
+// 	var outputStr string
+// 	var totalSize = dirMap[curDir]
+// 	for key, value := range dirMap {
+// 		s +=
+// 	}
+// }
+
+
+func CalcPercent(this int64, total int64) float64 {
+	result := float64(this) / float64(total)
+	return result
+}
+
+
 func main() {
-	startDir := "."
-	SubDirSizes(startDir)
+	curDir := "."
+	sizes, err := SubDirSizes(curDir)
+	if err != nil {
+			log.Fatal(err)
+	}
+	totalSize := sizes[curDir]
+	for key, value := range sizes {
+		fmt.Printf("%v %v: %v\n", key, value, CalcPercent(value, totalSize))
+	}
 }
